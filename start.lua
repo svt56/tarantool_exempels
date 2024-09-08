f205be397de17feb8ea7fd4f79e111a84043c218
svt56 <hubgitta@ro.ru>
2024-09-08 07:58:08 +0300

    Add files via upload

---
+++ start.lua
@@ -0,0 +1,43 @@
+#!/usr/bin/env tarantool
+-- выделяем 400 ГБ под мемх 429496729600, 
+box.cfg({memtx_memory = 329496729600})
+-- созадть пользователя
+box.schema.user.create('user', { password = 'user' })
+-- пердоставить права на все пользователю
+box.schema.user.grant('user','read,write,create,execute,session,alter,drop,usage','universe')
+-- блокируем guest
+
+-- пароль на администратора
+box.schema.user.passwd('admin', 'admin')
+-- мигрируем под юзера
+box.session.su('user')
+-- создаем базу данных speed
+box.schema.space.create('speed')
+-- определяем формат
+box.space.speed:format({
+                           { name = 'id', type = 'unsigned' },
+                           { name = 'phone', type = 'string' },
+                           { name = 'email', type = 'string' },
+                           { name = 'fio', type = 'string' },
+                           { name = 'f', type = 'string' },
+                           { name = 'n', type = 'string' },
+                           { name = 's', type = 'string' },
+                           { name = 'fidor', type = 'string' },
+                           { name = 'pass', type = 'string' },
+                           { name = 'grz', type = 'string' },
+                           { name = 'vk', type = 'string' },
+                           { name = 'vu', type = 'string' },
+                           { name = 'tg', type = 'string' },
+                           { name = 'tgid', type = 'string' },
+                           { name = 'fb', type = 'string' },
+                           { name = 'inn', type = 'string' },                                     
+                           { name = 'firm', type = 'string' },
+                           { name = 'base', type = 'string' },
+                           { name = 'adres', type = 'string' },
+                           { name = 'json', type = 'string' },
+
+                       })
+-- создаем счетчик
+box.schema.sequence.create('id_speed',{min=1})
+-- привязываем счетчик к полю id в таблице
+box.space.speed:create_index('id',{ sequence = 'id_speed' })
